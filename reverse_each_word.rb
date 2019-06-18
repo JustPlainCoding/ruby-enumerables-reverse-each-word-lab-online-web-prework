581498f1952786cfb0fb5f9a5b0ea2f49ea624e7
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-18 20:59:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  reversed = []
-  string_array.each do |element|
-    reversed << element.reverse
-    p reversed
+  reversed = string_array.collect do |element|
+    element.reverse
   end
   reversed.join(" ")
 end
